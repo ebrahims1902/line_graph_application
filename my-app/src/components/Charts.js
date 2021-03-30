8a8765e421941e6c5d6d3035cf58cdf62ba49c9a
Ebrahim <ebrahims1902@gmail.com>
2021-03-30 14:22:44 +0530

    Complete_Line_Graph_App

--- my-app/src/components/Charts.js
+++ my-app/src/components/Charts.js
@@ -1,30 +1,42 @@
-import { Chart } from "react-google-charts";
-import * as React from "react";
+import React, { Component } from "react";
+import Chart from "react-google-charts";
 
+const options = {
+  title: "Company Performance",
+  curveType: "function",
+  legend: {
+    position: 'bottom',
+  },
+  hAxis: {
+    format: 'short',
+    title: 'Date',              
+  },
+  vAxis: {
+    title: 'Sold',
+    format:'long',
+    viewWindow: { min: 0, max: 700 },
+  },
+  series: {
+    0: { color: '#e2431e' },
+  },
+  backgroundColor: '#f1f8e9',
+  lineWidth: 4,
+};
 
-function Charts() {
+class Charts extends Component {
+  render() {
     return (
+      <div>
         <Chart
-        width={'800px'}
-        height={'400px'}
           chartType="LineChart"
+          width="100%"
+          height="600px"
           spreadSheetUrl="https://docs.google.com/spreadsheets/d/1XsvTNTjPr0nNC1g3xkE_8G2R-6M-wYI4eTSsPwE9Z9w/edit?usp=sharing"
-  options={{
-    hAxis: {
-      format: 'short',
-      title: 'Date',
-    },
-    vAxis: {
-      
-      title: 'Sold',
-      // format:'scientific',
-      format:'long',
-    },
-  }}
-  rootProps={{ 'data-testid': '1' }}
-/>
+          options={options}
+        />
+      </div>
     );
   }
-  
-  export default Charts;
-  
+}
+
+export default Charts;
